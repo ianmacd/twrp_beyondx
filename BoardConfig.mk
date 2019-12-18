af620f84c18e638c1b43fc5ad3b4c984e08a2ea7
Ian Macdonald <ian@caliban.org>
2019-12-18 16:27:48 +0100

    Version 3.3.1-7_ianmacd.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Version 3.3.1-6_ianmacd.
+# Version 3.3.1-7_ianmacd.
 
 # Architecture
 TARGET_ARCH := arm64
@@ -67,4 +67,4 @@ TW_NO_LEGACY_PROPS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := device/samsung/beyondx/prebuilt/recovery_dtbo
-PLATFORM_SECURITY_PATCH := 2019-08-20
+PLATFORM_SECURITY_PATCH := 2019-11-01
